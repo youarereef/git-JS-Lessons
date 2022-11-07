b8f9cb54c4fcd2cee596bfeb3bca3d5df77adc8a
youarereef <yureef@gmail.com>
2022-11-07 19:39:40 +0300

    test

---
+++ deck-of-cards/deck.js
@@ -0,0 +1,11 @@
+let suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
+
+class Cards {
+    constructor(suit, value) {
+        this.suit = suit;
+        this.value = value;
+    }
+}
+
+let card = newCard('Hearts', 8);
